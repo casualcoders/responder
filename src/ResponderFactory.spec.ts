8b0f4215597cb56a760d17acb3bd038b5ea07240
Camiloruiiz <camiloruiz@esquemacreativo.com>
2020-07-08 20:12:31 +0100

    Add createsResponders method and test around Co-authored-by: Jinja144 <benjamin.watson.1995@gmail.com>

--- src/ResponderFactory.spec.ts
+++ src/ResponderFactory.spec.ts
@@ -2,23 +2,67 @@ import should from 'should';
 import ResponderFactory from './ResponderFactory';
 import Responder from './Responder';
 
-const BREAKPOINTS = {
-    sm: { label: 'small', enter: 0, exit: 575 },
-    mobile: { label: 'mobile', enter: 576, exit: 767 },
-    tablet: { label: 'tablet', enter: 768, exit: 991 },
-    desktop: { label: 'desktop', enter: 992, exit: 1199 },
-    xl: { label: 'big', enter: 1200, exit: 9999 }
-}
+const CONF = [
+    { label: 'small', min: 0, max: 575 },
+    { label: 'mobile', min: 576, max: 767 },
+    { label: 'tablet', min: 768, max: 991 },
+    { label: 'desktop', min: 992, max: 1199 },
+    { label: 'big', min: 1200, max: 9999 }
+]
 describe('ResponderFactory', () => {
     describe('On new instance creation', () => {
         it('It should have a breakpoint property equal to the parameter', () => {
-            let responderFactory = new ResponderFactory(BREAKPOINTS)
-            should(responderFactory.breakpoints).be.equal(BREAKPOINTS);
+            let responderFactory = new ResponderFactory(CONF)
+            should(responderFactory.breakpoints).be.equal(CONF);
+        })
+    })
+    
+    describe('On the createResponder method call', () => {
+        it('It should return an instance of Responder Class', () => {
+            let responderFactory = new ResponderFactory(CONF)
+            const responder = responderFactory.createResponder([], () => {}, () => {})
+            should(typeof(responder)).be.equal(typeof(new Responder([], [],  () => {}, () => {} )))
         })
 
-        it('It should return an instance of Responder Class', () => {
-            let responderFactory = new ResponderFactory(BREAKPOINTS)
-            should(typeof(responderFactory)).be.equal(typeof(new Responder()))
+        it('A responder should be setup with the parameters passed to the method', () => {
+            let responderFactory = new ResponderFactory(CONF)
+            let viewports = ['test']
+            const exitFn = jest.fn()
+            const enterFn = jest.fn()
+            const responder = responderFactory.createResponder(viewports, enterFn, exitFn)
+            should(responder.config).be.equal(CONF);
+            should(responder.viewports).be.equal(viewports);
+            should(responder.enterFunction).be.equal(enterFn);
+            should(responder.exitFunction).be.equal(exitFn);
         })
+    })
+
+    describe('On the createResponders method call', () => {
+        it('It should return an array of instances of Responder Classes', () => {
+            let responderFactory = new ResponderFactory(CONF)
+            const responderConfs = [{viewports: [], enterFn: () => {}, exitFn: () => {}},{viewports: [], enterFn: () => {}, exitFn: () => {}}]
+            const responders = responderFactory.createResponders(responderConfs)
+            should(typeof(responders)).be.equal(typeof([]))
+            should(responders.length).be.equal(responderConfs.length)
+            for (let index = 0; index < responders.length; index++) {
+                const responder = responders[index];
+                should(typeof(responder)).be.equal(typeof(new Responder([], [],  () => {}, () => {} )))
+            }
+        })
+
+        it('The returned responders should be setup with the parameters passed using the responderConfig', () => {
+            let responderFactory = new ResponderFactory(CONF)
+            let viewports = ['test']
+            const exitFn = jest.fn()
+            const enterFn = jest.fn()
+            const responderConfs = [{viewports: viewports, enterFn: enterFn, exitFn: exitFn},{viewports: viewports, enterFn: enterFn, exitFn: exitFn}]
+            const responders = responderFactory.createResponders(responderConfs)
+            responders.forEach(responder => {
+                should(responder.config).be.equal(CONF);
+                should(responder.viewports).be.equal(viewports);
+                should(responder.enterFunction).be.equal(enterFn);
+                should(responder.exitFunction).be.equal(exitFn);
+            });
+        });
     })
 })
--- src/ResponderFactory.ts
+++ src/ResponderFactory.ts
@@ -1,13 +1,22 @@
 import Responder from './Responder'
-
 class ResponderFactory {
-    breakpoints: Array < any >
+    breakpoints: Array < breakpoint >
 
-    constructor(breakpoints: Array < any >) {
+    constructor(breakpoints: Array < breakpoint >) {
         this.breakpoints = breakpoints;
     }
-    createResponder(viewports: Array < String > , enterFunction: any, exitFunction: any) {
-        return new Responder(viewports, this.config, enterFunction, exitFunction)
+    createResponder(viewports: Array < String > , enterFunction: any, exitFunction: any): Responder {
+        return new Responder(viewports, this.breakpoints, enterFunction, exitFunction)
+    }
+
+    createResponders(responderConfigs: Array < ResponderConfig >): Array < Responder > {
+        let ResponderInstances: Array < Responder > = []
+
+        responderConfigs.forEach(responderConfig => {
+            ResponderInstances.push(new Responder(responderConfig.viewports, this.breakpoints, responderConfig.enterFunction, responderConfig.exitFunction)) 
+        });
+        
+        return ResponderInstances
     }
 }
 
